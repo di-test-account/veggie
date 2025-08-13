72ef1ec0c004f03c9c69ea72cb217e5a4ebc2eb8
di-test-account <sftrial@diintegrators.com>
2025-08-13 13:14:42 -0400

    Create insert_veggie_data.sql

---
+++ sql/sample_data/insert_veggie_data.sql
@@ -0,0 +1,6 @@
+-- Sample veggie data
+INSERT INTO GITHUB.RAW.VEGGIE (name, type, color, season) VALUES
+('Carrot', 'Root', 'Orange', 'Fall'),
+('Spinach', 'Leafy', 'Green', 'Spring'),
+('Tomato', 'Fruit', 'Red', 'Summer'),
+('Broccoli', 'Cruciferous', 'Green', 'Fall');
